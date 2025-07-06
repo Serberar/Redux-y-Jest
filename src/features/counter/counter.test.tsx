fbf004634ffd263d9a39d7aa4afa108262b30127
Sergio <davorsg@gmail.com>
2025-07-06 15:55:37 +0200

    Ampliados tests del contador: añadidos casos para incrementar en 5 y decrementar. Probados y funcionando correctamente.

--- src/features/counter/counter.test.tsx
+++ src/features/counter/counter.test.tsx
@@ -3,19 +3,40 @@ import Counter from './Counter';
 import { Provider } from 'react-redux';
 import { setupStore } from '../../app/setupStore';
 
+function renderWithStore() {
+  const store = setupStore();
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+  return store;
+}
+
 describe('Counter', () => {
   test('incrementa el contador', () => {
-    const store = setupStore();
-
-    render(
-      <Provider store={store}>
-        <Counter />
-      </Provider>
-    );
+    renderWithStore();
 
     fireEvent.click(screen.getByText('+1'));
     fireEvent.click(screen.getByText('+1'));
 
     expect(screen.getByText('Contador: 2')).toBeInTheDocument();
+  });
+
+  test('incrementa el contador en 5', () => {
+    renderWithStore();
+
+    fireEvent.click(screen.getByText('+5'));
+
+    expect(screen.getByText('Contador: 5')).toBeInTheDocument();
+  });
+
+  test('decrementa el contador', () => {
+    renderWithStore();
+
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('-1'));
+
+    expect(screen.getByText('Contador: 0')).toBeInTheDocument();
   });
 });
